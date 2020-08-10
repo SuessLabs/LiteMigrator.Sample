9b6d600422caf29b48e0f4e5bee61bda97d2a964
Damian Suess <djsuess@gmail.com>
2020-08-10 11:17:10 -0400

    base project from https://github.com/xenoinc/XenoXamarinTemplates

---
+++ LiteMigrator.Sample/LiteMigrator.Sample.Client/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace LiteMigrator.Sample.Client.Views
+{
+  public partial class MainView : ContentPage
+  {
+    public MainView()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ LiteMigrator.Sample/LiteMigrator.Sample.UWP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism;
+using Prism.Ioc;
+
+namespace LiteMigrator.Sample.UWP
+{
+  public sealed partial class MainPage
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+
+      LoadApplication(new LiteMigrator.Sample.UWP.Client.App(new UwpInitializer()));
+    }
+  }
+
+  public class UwpInitializer : IPlatformInitializer
+  {
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+      // Register any platform specific implementations
+    }
+  }
+}
